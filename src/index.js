8b36b70eaede02d958d30c5813bbbd9179ad6f91
Tushar Srivastava <tusharsrivastava2003@gmail.com>
2025-02-20 02:07:36 +0530

    set default port to 8080 if not specified in environment variables

--- src/index.js
+++ src/index.js
@@ -21,5 +21,5 @@ app.get("/", (req, res) => {
 });
 
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
