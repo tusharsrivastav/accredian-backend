1262d893093fd0a07f782ce336007a3d4bee853e
Tushar Srivastava <tusharsrivastava2003@gmail.com>
2025-02-20 01:38:33 +0530

    enhance referral validation messages for clarity and add validation for referee details

--- src/middlewares/validateReferral.js
+++ src/middlewares/validateReferral.js
@@ -1,14 +1,16 @@
 import { body, validationResult } from "express-validator";
 
 const validateReferral = [
-  body("referrerName").notEmpty().withMessage("Name is required"),
-  body("referrerEmail").isEmail().withMessage("Invalid email address"),
-  body("referrerName").notEmpty().withMessage("Name is required"),
-  body("referrerEmail").isEmail().withMessage("Invalid email address"),
+  body("referrerName").notEmpty().withMessage("Your Name is required"),
+  body("referrerEmail").isEmail().withMessage("Your email address is invalid"),
+  body("refereeName").notEmpty().withMessage("Your friend's Name is required"),
+  body("refereeEmail").isEmail().withMessage("Friend's email address is invalid"),
   
   (req, res, next) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
+      console.log(errors.array());
+      
       return res.status(400).json({ errors: errors.array() });
     }
     next();
